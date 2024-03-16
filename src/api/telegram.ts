e60277e5ad25199b0f1361230bf1caf945670578
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2024-03-16 19:21:18 +0200

    change names

--- src/api/telegram.ts
+++ src/api/telegram.ts
@@ -1,6 +1,6 @@
-const baseUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/`;
+const baseUrl = `https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN}/`;
 export const sendMessage = async (message: string): Promise<void> => {
-  const url = `${baseUrl}sendMessage?chat_id=${process.env.CHAT_ID}&text=${message}`;
+  const url = `${baseUrl}sendMessage?chat_id=${process.env.NEXT_PUBLIC_CHAT_ID}&text=${message}`;
 
   const response = await fetch(url);
   console.log(response);
